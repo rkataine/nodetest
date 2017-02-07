6726103e0608eb72b80646a31027c7d671cea855
rkataine <riku.katainen@helsinki.fi>
2017-02-07 21:03:34 +0200

    test

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT ||8080;
+//var port = process.env.PORT ||8080;
+var port = 80;
 var server = app.listen(port);
 
 app.use(express.static('public'));
